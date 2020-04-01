17d748762666017001c1550ded57e26bce0894db
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-04-01 16:50:08 +0530

    Sample added

---
+++ Sample-WPF/ViewModel.cs
@@ -0,0 +1,134 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Sample_WPF
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            
+        }
+    }
+
+    public class DataGenerator
+    {
+        public int DataCount = 50000;
+        private int RateOfData = 5;
+        private ObservableCollection<Data> Data;
+        private Random randomNumber;
+        int myindex = 0;
+        DispatcherTimer timer;
+
+        public ObservableCollection<Data> DynamicData { get; set; }
+
+        public DataGenerator()
+        {
+            randomNumber = new Random();
+            DynamicData = new ObservableCollection<Data>();
+            Data = new ObservableCollection<Data>();
+            Data = GenerateData();
+            LoadData();
+
+            timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            timer.Start();
+        }
+
+        public void AddData()
+        {
+            for (int i = 0; i < RateOfData; i++)
+            {
+                myindex++;
+                if (myindex < 100)
+                {
+                    DynamicData.Add(this.Data[myindex]);
+                }
+                else if (myindex > 100)
+                {
+                    DynamicData.RemoveAt(0);//Remove data not visible
+                    DynamicData.Add(this.Data[(myindex % (this.Data.Count - 1))]);
+                }
+            }
+        }
+
+        public void LoadData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                myindex++;
+                if (myindex < Data.Count)
+                {
+                    DynamicData.Add(this.Data[myindex]);
+                }
+            }
+        }
+
+        public ObservableCollection<Data> GenerateData()
+        {
+            ObservableCollection<Data> datas = new ObservableCollection<Data>();
+
+            DateTime date = new DateTime(2009, 1, 1);
+            double value = 1000;
+            double value1 = 1001;
+            double value2 = 1002;
+            for (int i = 0; i < this.DataCount; i++)
+            {
+                datas.Add(new Data(date, value, value1, value2));
+                date = date.Add(TimeSpan.FromSeconds(5));
+
+                if ((randomNumber.NextDouble() + value2) < 1004.85)
+                {
+                    double random = randomNumber.NextDouble();
+                    value += random;
+                    value1 += random;
+                    value2 += random;
+                }
+                else
+                {
+                    double random = randomNumber.NextDouble();
+                    value -= random;
+                    value1 -= random;
+                    value2 -= random;
+                }
+            }
+
+            return datas;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            AddData();
+        }
+    }
+
+    public class Data
+    {
+        public Data(DateTime date, double value, double value1, double value2)
+        {
+            Date = date;
+            Value = value;
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        public DateTime Date { get; set; }
+
+        public double Value { get; set; }
+
+        public double Value1 { get; set; }
+
+        public double Value2 { get; set; }
+    }
+}
